e109d0043c5eb0c59a2df3518cf29745b3195308
stasgovera <stanislavgovera@gmail.com>
2022-07-06 21:41:36 +0100

    First Controller

---
+++ SuperShop/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SuperShop.Data.Entities;
+
+namespace SuperShop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+    }
+}
