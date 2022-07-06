757771aeed4fdf771fc503c8542eba5fdfdffa02
stasgovera <stanislavgovera@gmail.com>
2022-07-06 23:53:54 +0100

    Modify (products)

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -11,6 +11,8 @@ namespace SuperShop.Data.Entities
         [Key]
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString ="{0:C2}",ApplyFormatInEditMode =false)]
@@ -20,10 +22,10 @@ namespace SuperShop.Data.Entities
         public string ImageURL { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
